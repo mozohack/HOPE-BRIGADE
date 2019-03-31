e151244a37295d6978b1d4d579e4bb73da579f7f
Sudarshan013 <39631554+Sudarshan013@users.noreply.github.com>
2019-03-31 14:44:45 +0530

    Update 3.Consumer.js

--- 3.Consumer.js
+++ 3.Consumer.js
@@ -1,47 +1,27 @@
-import React, { Component } from 'react'
-import { Button, Card,  CardGroup, Col, Container, Form,  InputGroup,   Row ,FormGroup,FormLabel} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React,{Component}  from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter as Link} from "react-router-dom";
+class Track extends Component{
+    static async getInitialProps(){
+        const accounts=await web3.eth.getAccounts();
+       
 
- class Consumer extends Component {
-    constructor(props){
-        super(props)
-            this.proceed=this.proceed.bind(this);
-            this.state={
-                drugid:''
-            };
-        };
-        proceed(){
-            console.log('this.state',this.state);
-        }
-        
     
-  render() {
-    return (
-      <div>
-         <div>
-        <center>TRACK DETAILS</center>
-      </div>
-      <Card>
+       const  PathOfDrug=await wmtSystem.methods.getSupplyChainDetails().call();
+    }
     
-     <center>
-      <Form className="form-vertical">
-        <Col>
-          <FormGroup><FormLabel className="control-label col-sm-2">DRUG ID</FormLabel>
-<input
-  type="text"
-  name="drugid"
-  
-   onChange={event =>this.setState({drugid:event.target.value})}
-/> </FormGroup>
+    render(){
+        return(
+            <div>
+                <Label>Enter Drug ID:</Label>
+                <input type="text"></input>
+            <Link route="/TrackDet"><button>Track Details</button></Link>    
+            <div>
+                {PathOfDrug}
+            </div>
+            </div>
+        )
+    }
 
-<Button onClick={this.proceed}>PROCEED</Button>
-
-</Col>
-</Form>
-</center>
-</Card>
-</div>
-)
 }
-}
-export default Consumer;
+export default Track;
