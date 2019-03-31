8ec25b19752b5ddc15709069af4c177853071ffe
Sudarshan013 <39631554+Sudarshan013@users.noreply.github.com>
2019-03-31 08:54:19 +0530

    Update kyd.sol

--- kyd.sol
+++ kyd.sol
@@ -87,14 +87,14 @@ contract knowYourDrug
          // pathOfDrug[pathTraceIndex]:supplyChainParts[currentAccessor].recogId,//
          
          
-         /*   function updatePath(uint32 supplyChainParts[currentAccessor].recogId) public
+           function updatePath(uint32 supplyChainParts[currentAccessor].recogId) public
             {
          
             pathOfDrug.push(supplyChainParts[currentAccessor].recogId)
             
             }
             
-         */   
+          
             
             manufactureDate: _manufactureDate,
             expiryDate: _expiryDate,
@@ -106,6 +106,85 @@ contract knowYourDrug
     }
     
     
+     function manufacturerUpdateDrugs(uint32 rid,string memory _drugName,string memory _drugId, uint32 _serialNumberFrom,uint32 _serialNumberTo,string memory _manufactureDate,string memory _expiryDate) public
+    {
+         require(accounts[rid]);
+         require(supplyChainParts[currentAccessor].isManufacturer);
+       // uint32[] memory tempPath;
+       // tempPath[0] = supplyChainParts[currentAccessor].recogId;
+       
+      
+        drugsInNetwork.push(DrugDetails({
+            drugName: _drugName,
+            manufacturedBy: supplyChainParts[currentAccessor].corresName,
+            drugId: _drugId,
+            serialNumberFrom: _serialNumberFrom,
+            serialNumberTo: _serialNumberTo,
+           // pathOfDrug:tempPath,
+           // pathOfDrug[pathTraceIndex]:supplyChainParts[currentAccessor].recogId,//
+            manufactureDate: _manufactureDate,
+            expiryDate: _expiryDate,
+            isFromAuthorisedSource: true
+
+            
+            
+        })
+        );
+        drugAccounts[_drugId]=true;
+        
+    }
+    
+    struct Supplier
+    {
+        uint32 recogId;
+        string drugId;
+        uint32 havingSerialNumberFrom;
+        uint32 havingSerialNumberTo;
+        string checkInDate;
+        string checkOutDate;
+        bool received;
+    }
+    Supplier[] public Suppliers;
+    
+    function supplierUpdateDrugs(uint32 _ID,string memory _drugId,uint32 _serialNumberFrom,uint32 _serialNumberTo, string memory _checkInDate, string memory _checkOutDate) public
+    {
+        require(drugAccounts[_drugId]);
+        require(accounts[_ID]);
+        require(supplyChainParts[currentAccessor].isManufacturer==false);
+       for(uint32 index=0;index<drugsInNetwork.length;index++)
+        {
+            string memory t = drugsInNetwork[index].drugId;
+            if(keccak256(bytes(t))==keccak256(bytes(_drugId)))
+            {
+                currentDrugAccessed=index;
+                break;
+            }
+            else
+            {
+                drugException=true;
+                break;
+            }
+        }
+        if(drugException==false){
+           // uint32[] memory tempPath;
+           // tempPath[0] = supplyChainParts[currentAccessor].recogId;
+            
+            Suppliers.push(Supplier({
+                recogId:supplyChainParts[currentAccessor].recogId,
+                drugId:_drugId,
+                havingSerialNumberFrom:_serialNumberFrom,
+                havingSerialNumberTo:_serialNumberTo,
+                checkInDate:_checkInDate,
+                checkOutDate:_checkOutDate,
+                received: true
+            //have to include his recogId in the pathOfDrug of the drug
+            }));
+            
+            //drugsInNetwork[currentDrugAccessed].pathOfDrug = tempPath;
+        }
+        
+        
+    }
     
     
     
